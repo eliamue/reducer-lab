e4307f4e71b11c74b1dae5c13cbb37522d6e18ac
Elise Muellerleile <eliamue@live.com>
2021-09-30 11:38:16 -0500

    init

---
+++ src/components/app/App.test.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { render, screen, waitFor, fireEvent } from '@testing-library/react';
+import App from './App';
+
+const red = 'rgb(84, 24, 18)'; //#541812
+const blue = 'rgb(0, 51, 235)'; //#0033eb
+const green = 'rgb(59, 181, 3)'; //#3bb503
+const yellow = 'rgb(255, 236, 0)'; //#ffec00
+
+describe('App', () => {
+  it('Changes color of swatch', () => {
+    const { container } = render(<App />);
+    expect(container).not.toBeEmptyDOMElement();
+
+    const swatch = screen.getByRole('display', { name: 'swatch' });
+    expect(swatch.style.backgroundColor).toEqual(red);
+
+    const colorpick = screen.getByRole('color', { name: 'color-input' });
+    fireEvent.change(colorpick, blue);
+    waitFor(() => expect(colorpick).toHaveStyle({ 'background-color': blue }));
+
+    const color2 = screen.getByRole('color', { name: 'color-input' });
+    fireEvent.change(color2, green);
+    waitFor(() => expect(color2).toHaveStyle({ 'background-color': green }));
+
+    const color3 = screen.getByRole('color', { name: 'color-input' });
+    fireEvent.change(color3, yellow);
+    waitFor(() => expect(color3).toHaveStyle({ 'background-color': yellow }));
+  });
+
+  it('Changes color of swatch back to previously chosen color after hitting the undo button', () => {
+    render(<App />);
+
+    const undo = screen.getByRole('unbutton', { name: 'undo-button' });
+    const prevcolor = screen.getByRole('color', { name: 'color-input' });
+
+    fireEvent.change(prevcolor, yellow);
+    fireEvent.change(prevcolor, green);
+    fireEvent.change(prevcolor, blue);
+    fireEvent.click(undo);
+    return waitFor(() =>
+      expect(prevcolor).toHaveStyle({ 'background-color': 'ButtonFace' })
+    );
+  });
+
+  it('Changes color of swatch back to color previously selected before hitting the undo button, by clicking the redo button', () => {
+    render(<App />);
+
+    const redo = screen.getByRole('rebutton', { name: 'redo-button' });
+    const prevcolor = screen.getByRole('color', { name: 'color-input' });
+
+    fireEvent.change(prevcolor, yellow);
+    fireEvent.change(prevcolor, green);
+    fireEvent.change(prevcolor, blue);
+    fireEvent.click(redo);
+    return waitFor(() =>
+      expect(prevcolor).toHaveStyle({ 'background-color': 'ButtonFace' })
+    );
+  });
+});
---
+++ src/components/useRecord.js
@@ -0,0 +1,39 @@
+import { useEffect, useState } from 'react';
+
+export const useRecord = (initialColor = '#541812') => {
+  const [current, setCurrent] = useState(initialColor);
+  const [history, setHistory] = useState([initialColor]);
+  const [index, setIndex] = useState(0);
+  const [isDisabled, setDisabled] = useState(false);
+
+  const record = (newColor) => {
+    setHistory((prev) => [
+      ...prev.slice(0, index + 1),
+      newColor,
+      ...prev.slice(index + 1),
+    ]);
+
+    setCurrent(newColor);
+    setDisabled(false);
+    console.log(history);
+  };
+
+  const undo = () => {
+    if (index >= 0) setCurrent(history[index - 1]);
+    if (index === 1) setDisabled(true);
+  };
+
+  const redo = () => {
+    if (index <= history.length)
+      setCurrent(history[index + 1]) && setDisabled(false);
+    // if (index === history.length) setDisabled(true);
+  };
+
+  useEffect(() => {
+    setIndex(history.indexOf(current));
+  }, [current]);
+  //     setHistory((prev) => [...prev, current]);
+  //   }, [current]);
+
+  return { undo, redo, current, record, history, index, isDisabled };
+};
